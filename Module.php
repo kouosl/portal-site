9ad82d1b69ba99b0135d5fa2bd18d191919c0bc5
sh4deofsun <by.yalcin.batuhan@gmail.com>
2019-04-25 16:46:04 +0300

    deleting duplicates

--- Module.php
+++ Module.php
@@ -9,56 +9,15 @@ use yii\filters\auth\HttpBearerAuth;
 use yii\filters\auth\QueryParamAuth;
 use yii\web\HttpException;
 
-class Module extends \kouosl\base\Module
-{
+class Module extends \kouosl\base\Module{
     public $controllerNamespace = '';
 
-    public function init()
-    {
+    public function init(){
         parent::init();
-        // custom initialization code goes here
+        $this->registerTranslations();
     }
 
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-        switch ($this->namespace) {
-            case 'backend': {
-
-            };
-                break;
-            case 'frontend': {
-
-            };
-                break;
-            case 'api': {
-
-                $behaviors['authenticator'] = [
-                    'class' => CompositeAuth::className(),
-                    'authMethods' => [
-                        HttpBasicAuth::className(),
-                        HttpBearerAuth::className(),
-                        QueryParamAuth::className(),
-                    ],
-                ];
-            };
-                break;
-            case 'console': {
-
-            };
-                break;
-            default: {
-                throw new HttpException(500, 'behaviors' . $this->namespace);
-            };
-                break;
-        }
-
-        return $behaviors;
-
-    }
-
-    public function registerTranslations()
-    {
+    public function registerTranslations(){
         Yii::$app->i18n->translations['site/*'] = [
             'class' => 'yii\i18n\PhpMessageSource',
             'sourceLanguage' => 'en-US',
@@ -69,10 +28,7 @@ class Module extends \kouosl\base\Module
         ];
     }
 
-    public static function t($category, $message, $params = [], $language = null)
-    {
+    public static function t($category, $message, $params = [], $language = null){
         return Yii::t('site/' . $category, $message, $params, $language);
     }
-
-
 }
