9f3025f5d75d6d7e720c9c768894bd8ef93167e5
Gizem AYDIN <giizemaydinn@gmail.com>
2019-12-18 22:39:57 +0300

    guncelleme

--- migrations/m180425_090819_setting.php
+++ migrations/m180425_090819_setting.php
@@ -20,6 +20,11 @@ class m180425_090819_setting extends Migration
             'setting_key' => $this->string(200)->notNull(),
 			'value' => $this->string(200)->notNull(),
         ], $tableOptions);
+	
+	$this->insert('setting', [
+            'setting_key' => 'home',
+            'value' => '1',
+        ]);
 
         $this->insert('setting', [
             'setting_key' => 'signup',
@@ -43,7 +48,7 @@ class m180425_090819_setting extends Migration
 
         $this->insert('setting', [
             'setting_key' => 'language',
-            'value' => 'en-US',
+            'value' => 'tr-TR',
         ]);
 
 	$this->insert('setting', [
