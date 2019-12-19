d2b99191ff3c78916f35227364ce80fbb6301334
Gizem AYDIN <giizemaydinn@gmail.com>
2019-12-19 22:54:07 +0300

    language and home bug fixed

--- migrations/m180425_090819_setting.php
+++ migrations/m180425_090819_setting.php
@@ -21,6 +21,11 @@ class m180425_090819_setting extends Migration
 			'value' => $this->string(200)->notNull(),
         ], $tableOptions);
 
+	$this->insert('setting', [
+            'setting_key' => 'home',
+            'value' => '1',
+        ]);
+
         $this->insert('setting', [
             'setting_key' => 'signup',
             'value' => 'true',
@@ -43,7 +48,7 @@ class m180425_090819_setting extends Migration
 
         $this->insert('setting', [
             'setting_key' => 'language',
-            'value' => 'en-US',
+            'value' => 'tr-TR',
         ]);
 
 	$this->insert('setting', [
